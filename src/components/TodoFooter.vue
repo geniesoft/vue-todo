b3f1cefab4a78b8bce885d37c514c50c9f9bbfc2
geniesoft <genie@geniesoft.io>
2019-05-09 10:26:06 +0900

    defalt
    default

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,11 +1,30 @@
 <template>
-  <div> footer </div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">모두 삭제</span>
+  </div>
 </template>
 
 <script>
-
 export default {
-
+  methods: {
+    clearTodo() {
+      this.$emit('removeAll');
+    }
   }
-
+}
 </script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
+</style>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,11 +1,18 @@
 <template>
-  <div> header </div>
+  <header>
+    <h1>오늘의 할일</h1>
+  </header>
 </template>
 
 <script>
-
 export default {
-
-  }
-
+}
 </script>
+
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
